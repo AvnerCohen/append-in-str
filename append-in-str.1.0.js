a0c9cb7506a858a52f4d62bf8ea2ad9dce16c769
Avner Cohen <israbirding@gmail.com>
2012-08-05 11:08:45 +0300

    Testings completed

--- append-in-str.1.0.js
+++ append-in-str.1.0.js
@@ -8,8 +8,8 @@
             return origStr;
         }
         //trim append Str as needed.
-        if ((origStr + appendStr).length > maxLength) {
-            appendStr = appendStr.substr(0, appendStr.length - ((origStr + appendStr).length - maxLength));
+        if (maxLength != null && (origStr + appendStr).length > maxLength) {
+            appendStr = appendStr.substr(0, (appendStr.length-1) - ((origStr + appendStr).length - maxLength));
         }
         //Simple case, no injection
         if (position == null) {
@@ -19,6 +19,5 @@
         var part1 = origStr.substr(0, position);
         var part2 = origStr.substr(position, origStr.length);
         return (part1 + appendStr + part2);
-
     }
 })(jQuery);
